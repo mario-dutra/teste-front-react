f358f6b875e032e69690389152be889b1e8ee19c
mario-dutra <134343932+mario-dutra@users.noreply.github.com>
2025-09-15 16:38:51 -0300

    Update Header.jsx

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -7,7 +7,7 @@ import React from "react";
 
        <header id="topo">
         <div className="img-logo">
-            <img className="logo" src="logo_sa.png" />
+            <img className="logo" src="assets/img/logo_sa.png" />
         </div>
       
 
